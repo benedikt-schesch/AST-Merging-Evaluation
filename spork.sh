89e7638481ed18b6fec4a106250079f140dff902
ryanfeatherman <ryan.featherman3@gmail.com>
2022-11-15 21:26:31 -0800

    fixed ret vals for spork and git merge

--- spork.sh
+++ spork.sh
@@ -6,11 +6,23 @@ rightdir=$1/right
 outputdir=$2
 
 find $basedir -name "*.java" -or -name "**/*.java"|while read basename; do
+    # construct paths
     suffix=${basename#"$basedir"}
     leftname=$leftdir$suffix
     rightname=$rightdir$suffix
     outputname=$outputdir$suffix
+
+    # create output location
     mkdir -p $(dirname $outputname)
     touch $outputname
+
+    # run spork
     java -jar $SPORK -o=$outputname $leftname $basename $rightname
+
+    # report conflicts
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        echo "Conflict"
+        exit $retVal
+    fi
 done
