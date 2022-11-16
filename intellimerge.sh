d2d6032ca44b256f64a7e5d5c775f66083905e6d
ryanfeatherman <ryan.featherman3@gmail.com>
2022-11-15 22:29:23 -0800

    intellimerge for branches

--- intellimerge.sh
+++ intellimerge.sh
@@ -1,15 +1,16 @@
-# usage: ./intellmerge.sh <merge-dir> <output-dir>
+# usage: ./intellmerge.sh <repo> <branch1> <branch2> <outputdir>
 INTELLIMERGE=./jars/IntelliMerge-1.0.9-all.jar
-leftdir=$1/left
-basedir=$1/base
-rightdir=$1/right
-outputdir=$2
+repo=$1
+branch1=$2
+branch2=$3
+outputdir=$4
 
-java -jar $INTELLIMERGE -d $leftdir $basedir $rightdir -o $outputdir
+# run intellimerge
+java -jar $INTELLIMERGE -r $repo -b $branch1 $branch2 -o $outputdir
 
-currentdir=$(pwd)
-mkdir temp
-mv output/$currentdir/* temp
-rm -r output/*/
-mv temp/* output
-rmdir temp
+# report conflicts
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Conflict"
+    exit $retVal
+fi
