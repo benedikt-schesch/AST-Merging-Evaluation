9a45b261b20bba813c8d4125900eb89db3ad0910
ryanfeatherman <ryan.featherman3@gmail.com>
2022-11-15 19:14:20 -0800

    fix dir doesn't exist

--- gitmerge.sh
+++ gitmerge.sh
@@ -5,10 +5,11 @@ rightdir=$1/right
 outputdir=$2
 
 find $basedir -name "*.java" -or -name "**/*.java"|while read basename; do
-    echo $basename
     suffix=${basename#"$basedir"}
     leftname=$leftdir$suffix
     rightname=$rightdir$suffix
     outputname=$outputdir$suffix
+    mkdir -p $(dirname $outputname)
+    touch $outputname
     git merge-file -p $leftname $basename $rightname > $outputname
 done
