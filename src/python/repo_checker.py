08f32d0b1328a6d7fa4128af80c025accfc0ef6e
Benedikt <scheschb@student.ethz.ch>
2022-11-17 18:07:22 -0800

    Restructuring + Parent Merge Checker

---
+++ src/python/repo_checker.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from git import Repo
+import subprocess
+import shutil
+import os
+import multiprocessing
+from pebble import ProcessPool
+
+def check_repo(arg):
+    idx,row = arg
+    repo_name = row["repository"]
+
+    repo_dir = "repos/"+repo_name
+    target_file = "cache/result/"+repo_name.replace("/","_")+".txt"
+
+    if os.path.isfile(target_file):
+        return
+
+    with open(target_file,'a') as fp:
+        fp.write(str(1))
+
+    if not os.path.isdir(repo_dir):
+        git_url = "https://github.com/"+repo_name+".git"
+        Repo.clone_from(git_url, repo_dir)
+    pwd = os.getcwd()
+    rc = subprocess.run([pwd+"/src/scripts/tester.sh",repo_dir])
+
+    with open(target_file,'a') as fp:
+        fp.write(str(rc.returncode))
+
+
+if __name__ == '__main__':
+    df = pd.read_csv("data/repos.csv")
+
+    with ProcessPool() as pool:
+        future = pool.map(check_repo, df.iterrows(), timeout=10*60)
+
+        iterator = future.result()
+        while True:
+            try:
+                result = next(iterator)
+            except StopIteration:
+                break
+            except TimeoutError as error:
+                print("function took longer than %d seconds" % error.args[1])
+    
+    out = []
+    for idx,row in df.iterrows():
+        repo_name = row["repository"]
+        repo_dir = "repos/"+repo_name
+        target_file = "cache/result/"+repo_name.replace("/","_")+".txt"
+        if os.path.isfile(target_file):
+            with open(target_file) as fp:
+                if int(next(fp)) == 0:
+                    out.append(row)
+    out = pd.DataFrame(out)
+    out.to_csv("data/valid_repos.csv")
