9e740b68b6cf0290a1c7ba63fadb6de26a25be18
Benedikt <scheschb@student.ethz.ch>
2022-11-23 19:29:17 -0800

    Small Fix

--- run_small.sh
+++ run_small.sh
@@ -6,4 +6,6 @@ sh src/scripts/find_merge_commits.sh data/valid_repos_small.csv merges_small
 
 python3 src/python/test_parent_merges.py --repos_path data/valid_repos_small.csv --merges_path merges_small/ --output_dir merges_small_valid/
 
-python3 src/python/merge_tester.py --repos_path data/valid_repos_small.csv --merges_path merges_small_valid/ --output_file data/result_small.csv
+python3 src/python/reduce_merges.py --merges_path merges_small_valid/ --output_dir merges_small_valid_subsamples/ --max_size 10
+
+python3 src/python/merge_tester.py --repos_path data/valid_repos_small.csv --merges_path merges_small_valid_subsamples/ --output_file data/result_small.csv
