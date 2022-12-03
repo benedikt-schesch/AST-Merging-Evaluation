af410183adda7765b4a9f12172b886291bfbf74c
Benedikt <scheschb@student.ethz.ch>
2022-12-02 20:20:05 -0800

    Bug fix

--- src/python/test_parent_merges.py
+++ src/python/test_parent_merges.py
@@ -47,12 +47,11 @@ def pass_test(args):
     repo = git.Git(repo_dir_copy)
     repo.fetch()
     repo.checkout(commit)
-    pwd = os.getcwd()
 
     try:
         test = subprocess.run([command_timeout,
-                                TIMEOUT_SECONDS+"s",
-                                pwd+"/src/scripts/tester.sh",
+                                str(TIMEOUT_SECONDS)+"s",
+                                "src/scripts/tester.sh",
                                 repo_dir_copy]).returncode
     except Exception:
         test = 2
