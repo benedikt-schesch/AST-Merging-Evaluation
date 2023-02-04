b28015fdb90f250380be5a8f956e15079048fb74
Benedikt <scheschb@student.ethz.ch>
2023-02-04 14:04:22 -0800

    Script fix

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ python3 src/python/repo_checker.py --repos_path data/repos.csv --output_path res
 
 sh src/scripts/find_merge_commits.sh results/valid_repos.csv results/merges
 
-python3 src/python/reduce_merges.py --merges_path results/merges/ --output_dir results/merges_subsampled/ --max_merges 100
-
-python3 src/python/test_parent_merges.py --repos_path results/valid_repos.csv --merges_path results/merges_subsampled/ --output_dir results/merges_valid/ --num_cpu $1
+python3 src/python/test_parent_merges.py --repos_path results/valid_repos.csv --merges_path results/merges/ --output_dir results/merges_valid/ --num_cpu $1 --n_merges 100
 
 python3 src/python/merge_tester.py --repos_path results/valid_repos.csv --merges_path results/merges_valid/ --output_file results/result.csv --num_cpu $1
