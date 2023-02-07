3dc7861005a6cf8cfb33ff781448de8b3975a864
Michael Ernst <mernst@cs.washington.edu>
2023-02-07 12:55:18 -0800

    Add `run_cf.sh` for testing

---
+++ run_cf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# usage: ./run_small.sh
+# Runs the stack on two small repos.
+# The output appears in small/ .
+
+set -e 
+set -o nounset
+
+JAVA_VER=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+
+if [ "$JAVA_VER" != "8" ]; then
+  echo "Wrong Java version $JAVA_VER. Please use Java 8."
+  exit 1
+fi
+
+python3 src/python/repo_checker.py --repos_path data/repos_cf.csv --output_path small/valid_repos.csv
+
+sh src/scripts/find_merge_commits.sh small/valid_repos.csv small/merges_small
+
+python3 src/python/test_parent_merges.py --repos_path small/valid_repos.csv --merges_path small/merges_small/ --output_dir small/merges_small_valid/ --n_merges 2
+
+python3 src/python/merge_tester.py --repos_path small/valid_repos.csv --merges_path small/merges_small_valid/ --output_file small/result.csv
