172132316f3201a050f4543f7734a99cbae1f0e9
Michael Ernst <mernst@cs.washington.edu>
2023-02-07 10:44:59 -0800

    It's good style to quote variables in shell scripts

--- src/scripts/find_merge_commits.sh
+++ src/scripts/find_merge_commits.sh
@@ -26,15 +26,15 @@ GET_COMMITS_FROM_PR() {
         -H "Accept: application/vnd.github+json" \
         --method GET \
         --paginate \
-        /repos/$1/pulls/$2/commits
+        "/repos/$1/pulls/$2/commits"
 }
 
 
-if [ ! -d $2 ]; then
-    mkdir $2
+if [ ! -d "$2" ]; then
+    mkdir "$2"
 fi
 
-for REPO in ${VALID_REPOS[@]} 
+for REPO in "${VALID_REPOS[@]}"
 do
     # Getting merge commits of mainline and feature branches
     # For each repo in list of repo names:
@@ -49,21 +49,21 @@ do
     #           - output (branch_name,merge,parent1,parent2,base)"
 
     REPO_NAME=$(cut -d '/' -f2 <<< "$REPO")
-    rm -rf ./$REPO_NAME
+    rm -rf "./$REPO_NAME"
     
     # Skip repos that have already been analyzed
     FILE=$2/$REPO_NAME.csv
-    echo $FILE
+    echo "$FILE"
     if test -f "$FILE"; then
         continue
     fi
     
     # Header for $REPO_NAME.csv
     echo "branch_name,merge_commit,parent_1,parent_2,base_commit" \
-        > $2/$REPO_NAME.csv
+        > "$2/$REPO_NAME.csv"
 
-    git clone https://github.com/$REPO.git
-    cd $REPO_NAME
+    git clone "https://github.com/$REPO.git"
+    cd "$REPO_NAME"
 
     # Get all branches name
     BRANCHES=$(git branch -a --format="%(refname:lstrip=3)" \
@@ -81,36 +81,36 @@ do
     DEFAULT_BRANCH=$(git branch --show-current)
     {
         MERGE_COMMITS="$(git log --merges --pretty=format:"%H")"
-        for MERGE_COMMIT in ${MERGE_COMMITS[@]}
+        for MERGE_COMMIT in "${MERGE_COMMITS[@]}"
         do
-            MERGE_PARENTS=( $(git log --pretty=%P -n 1 $MERGE_COMMIT) )
+            MERGE_PARENTS=( $(git log --pretty=%P -n 1 "$MERGE_COMMIT") )
             MERGE_BASE=$(git merge-base \
-                        ${MERGE_PARENTS[0]} ${MERGE_PARENTS[1]})
+                        "${MERGE_PARENTS[0]}" "${MERGE_PARENTS[1]}")
             
-            echo "$DEFAULT_BRANCH,$MERGE_COMMIT,${MERGE_PARENTS[0]},${MERGE_PARENTS[1]},$MERGE_BASE" >> ../$2/$REPO_NAME.csv
+            echo "$DEFAULT_BRANCH,$MERGE_COMMIT,${MERGE_PARENTS[0]},${MERGE_PARENTS[1]},$MERGE_BASE" >> "../$2/$REPO_NAME.csv"
         done
     }
 
-    for BRANCH in ${BRANCHES[@]}
+    for BRANCH in "${BRANCHES[@]}"
     do
         # ignore master branch, commits already retrieved
-        if [[ $BRANCH != $DEFAULT_BRANCH ]]
+        if [[ "$BRANCH" != "$DEFAULT_BRANCH" ]]
         then
-            git checkout -B $BRANCH origin/$BRANCH
+            git checkout -B "$BRANCH" "origin/$BRANCH"
 
             # Get merge commits
             MERGE_COMMITS=$(git log --merges --pretty=format:"%H")
-            for MERGE_COMMIT in ${MERGE_COMMITS[@]}
+            for MERGE_COMMIT in "${MERGE_COMMITS[@]}"
             do
-                MERGE_PARENTS=( $(git log --pretty=%P -n 1 $MERGE_COMMIT) )
+                MERGE_PARENTS=( $(git log --pretty=%P -n 1 "$MERGE_COMMIT") )
                 MERGE_BASE=$(git merge-base \
-                            ${MERGE_PARENTS[0]} ${MERGE_PARENTS[1]})
+                            "${MERGE_PARENTS[0]}" "${MERGE_PARENTS[1]}")
                 COMMIT_TUPLE="$MERGE_COMMIT,${MERGE_PARENTS[0]},${MERGE_PARENTS[1]},$MERGE_BASE"
                 
                 # add commit tuple if not seen before
                 if ! grep --quiet "$COMMIT_TUPLE" "../$2/$REPO_NAME.csv"
                 then
-                    echo "$BRANCH,$MERGE_COMMIT,${MERGE_PARENTS[0]},${MERGE_PARENTS[1]},$MERGE_BASE" >> ../$2/$REPO_NAME.csv
+                    echo "$BRANCH,$MERGE_COMMIT,${MERGE_PARENTS[0]},${MERGE_PARENTS[1]},$MERGE_BASE" >> "../$2/$REPO_NAME.csv"
                 fi
             done
         fi
@@ -134,17 +134,17 @@ do
                     --method GET \
                     --paginate \
                     --jq '.[].number' \
-                    /repos/$REPO/pulls \
+                    "/repos/$REPO/pulls" \
                     -f state=all)
     
-    for PR_NUMBER in ${PULL_REQUESTS[@]}
+    for PR_NUMBER in "${PULL_REQUESTS[@]}"
     do
-        COMMITS=( $(GET_COMMITS_FROM_PR $REPO $PR_NUMBER | jq -r '.[].sha') )
+        COMMITS=( $(GET_COMMITS_FROM_PR "$REPO" "$PR"_NUMBER | jq -r '.[].sha') )
         for (( i=0; i < ${#COMMITS[@]}; i++ ))
         do
-            PARENTS_RESULT=( $(GET_COMMITS_FROM_PR $REPO $PR_NUMBER \
+            PARENTS_RESULT=( $(GET_COMMITS_FROM_PR "$REPO" "$PR_NUMBER" \
                             | jq --arg i "$i" '.[($i | tonumber)].parents') )
-            NUM_OF_PARENTS=$(GET_COMMITS_FROM_PR $REPO $PR_NUMBER \
+            NUM_OF_PARENTS=$(GET_COMMITS_FROM_PR "$REPO" "$PR_NUMBER" \
                             | jq --arg i "$i" '.[($i | tonumber)].parents | length')
             
             # A merge commit is found if it has two parents,
@@ -152,19 +152,19 @@ do
             if [[ $NUM_OF_PARENTS -eq 2 ]]
             then
                 MERGE_COMMIT=${COMMITS[$i]}
-                MERGE_PARENTS=( $(GET_COMMITS_FROM_PR $REPO $PR_NUMBER \
+                MERGE_PARENTS=( $(GET_COMMITS_FROM_PR "$REPO" "$PR_NUMBER" \
                                 | jq -r --arg i "$i" '.[($i | tonumber)].parents[].sha') )
                 
                 # Create a new local branch from PR_NUMBER in order to reference commits on PR
-                git fetch origin pull/$PR_NUMBER/head:$PR_NUMBER
-                git checkout -B $PR_NUMBER
+                git fetch origin "pull/$PR_NUMBER/head:$PR_NUMBER"
+                git checkout -B "$PR_NUMBER"
                 
-                MERGE_BASE=$(git merge-base ${MERGE_PARENTS[0]} ${MERGE_PARENTS[1]})
-                echo "$PR_NUMBER,$MERGE_COMMIT,${MERGE_PARENTS[0]},${MERGE_PARENTS[1]},$MERGE_BASE" >> ../$2/$REPO_NAME.csv
+                MERGE_BASE=$(git merge-base "${MERGE_PARENTS[0]}" "${MERGE_PARENTS[1]}")
+                echo "$PR_NUMBER,$MERGE_COMMIT,${MERGE_PARENTS[0]},${MERGE_PARENTS[1]},$MERGE_BASE" >> "../$2/$REPO_NAME.csv"
             fi
         done
     done
     
     cd ..
-    rm -rf $REPO_NAME/
+    rm -rf "$REPO_NAME"
 done
