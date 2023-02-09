ce919cc15ffc3b881c5b1607a56ed8240f404970
ryanfeatherman <ryan.featherman3@gmail.com>
2023-02-08 23:16:07 -0800

    minor code cleanup on plots.py

--- src/python/plots.py
+++ src/python/plots.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 import csv
 import os
 
+# make figures dir
 if not os.path.exists('../../figures'):
    os.makedirs('../../figures')
 
+# open results file
 with open('../../data/result.csv') as csvfile:
     reader = csv.reader(csvfile)
     rows = [row for row in reader]
@@ -15,9 +17,9 @@ with open('../../data/result.csv') as csvfile:
         row[8] = int(row[8])
         row[9] = int(row[9])
         row[10] = int(row[10])
-    rows = [row for row in rows if row[8]>0 and row[9]>0 and row[10]>0]
-    rows = [row for row in rows if (row[8] != 126 and row[9] != 126 and row[10] != 126) or row[8] == 2 or row[9] == 2 or row[10] == 2]
     
+    # drop error rows (-1 and -2)
+    rows = [row for row in rows if row[8]>0 and row[9]>0 and row[10]>0]
 
 gitmerge = [row[8] for row in rows]
 spork = [row[9] for row in rows]
@@ -38,8 +40,7 @@ intellimergecorrect = sum(val == 2 for val in intellimerge)
 intellimergeunhandled = sum(val == 1 for val in intellimerge)
 intellimergefailure = sum(val in [6,124] for val in intellimerge)
 
-#import pdb; pdb.set_trace()
-
+# figure 1 (stacked area)
 tools = ['Git Merge', 'Spork', 'IntelliMerge']
 incorrect = [gitmergeincorrect, sporkincorrect, intellimergeincorrect]
 unhandled = [gitmergeunhandled, sporkunhandled, intellimergeunhandled]
@@ -51,7 +52,6 @@ fig, ax = plt.subplots()
 ax.bar(tools, incorrect, label='Incorrect', color='#1F77B4')
 ax.bar(tools, unhandled, bottom=incorrect, label='Unhandled', color='#FF7F0E')
 ax.bar(tools, correct, label='Correct', bottom=[incorrect[i]+unhandled[i] for i in range(len(tools))], color='#2CA02C')
-#ax.bar(tools, failure, bottom=[correct[i]+unhandled[i]+incorrect[i] for i in range(len(tools))], label='Failure', color='red')
 
 ax.set_ylabel('# of merges')
 handles, labels = ax.get_legend_handles_labels()
@@ -59,6 +59,7 @@ ax.legend(reversed(handles), reversed(labels))
 
 plt.savefig('../../figures/stacked.pdf')
 
+# table 1 (overall results)
 template = """\\begin{{tabular}}{{c|c c|c c|c c}} 
          Tool & 
          \multicolumn{{2}}{{|c|}}{{Correct Merges}} & 
@@ -80,14 +81,13 @@ for i in range(len(tools)):
     args.append(100*unhandled[i]/total)
     args.append(incorrect[i])
     args.append(100*incorrect[i]/total)
-    #args.append(failure[i])
-    #args.append(100*failure[i]/total)
 
 table = template.format(*args)
 
 with open('../../figures/table1.txt', 'w') as file:
     file.write(table)
 
+# table 2 (by merge source)
 template2 = """\\begin{{tabular}}{{c|c c c c|c c c c|c c c c}} 
          Tool & 
          \multicolumn{{4}}{{|c|}}{{Correct Merges}} & 
@@ -141,8 +141,6 @@ for i in range(len(tools)):
     incorrect = sum(val in [3,5,126] for val in f[i])
     args.append(incorrect)
     args.append(100*incorrect/len(feature))
-
-import pdb; pdb.set_trace()
 
 table2 = template2.format(*args)
 
