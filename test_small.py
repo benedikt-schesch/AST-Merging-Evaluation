13ebd3b4796dd8adbd2d76b16cbd776b5de48b20
Benedikt <scheschb@student.ethz.ch>
2023-02-27 16:41:55 +0100

    pytest

--- test_small.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import pandas as pd
-
-def test_small():
-    subprocess.run(["./run_small.sh"],shell=True)
-    result = pd.read_csv("small/result.csv")
-    assert len(result) == 4 
