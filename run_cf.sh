10de8c4dd0f6d213645da0917a242a753feb2766
Benedikt <scheschb@student.ethz.ch>
2023-02-27 22:50:49 +0100

    Run_cf.py update

--- run_cf.sh
+++ run_cf.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# usage: ./run_small.sh
-# Runs the stack on two small repos.
-# The output appears in small/ .
+# usage: ./run_cf.sh
+# Runs the stack on the cf repos.
+# The output appears in cf/ .
+
+machine_id="${1:-0}"
+num_machines="${2:-1}"
+
+echo "Machine ID: $machine_id"
+echo "Number of machines: $num_machines"
 
 set -e 
 set -o nounset
@@ -14,10 +20,16 @@ if [ "$JAVA_VER" != "8" ]; then
   exit 1
 fi
 
-python3 src/python/repo_checker.py --repos_path data/repos_cf.csv --output_path small/valid_repos.csv
+mkdir -p cf
 
-sh src/scripts/find_merge_commits.sh small/valid_repos.csv small/merges_small
+python3 src/python/split_repos.py --repos_path data/repos_cf.csv --machine_id "$machine_id" --num_machines "$num_machines" --output_file cf/local_repos_cf.csv
 
-python3 src/python/test_parent_merges.py --repos_path small/valid_repos.csv --merges_path small/merges_small/ --output_dir small/merges_small_valid/ --n_merges 2
+python3 src/python/validate_repos.py --repos_path cf/local_repos_cf.csv --output_path cf/valid_repos.csv
 
-python3 src/python/merge_tester.py --repos_path small/valid_repos.csv --merges_path small/merges_small_valid/ --output_file small/result.csv
+./src/scripts/find_merge_commits.sh cf/valid_repos.csv cf/merges_cf
+
+python3 src/python/parent_merges_test.py --repos_path cf/valid_repos.csv --merges_path cf/merges_cf/ --output_dir cf/merges_cf_valid/ --n_merges 2
+
+python3 src/python/merge_tester.py --repos_path cf/valid_repos.csv --merges_path cf/merges_cf_valid/ --output_file cf/result.csv
+
+python3 src/python/latex_output.py --result_path small/result.csv --output_path small/plots
