882f73d820418c50d191b87c7d26a3abd3c14832
Benedikt Schesch <37979523+benedikt-schesch@users.noreply.github.com>
2023-03-08 11:32:37 +0100

    Uniform Run File

--- run.sh
+++ run.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# usage: ./run.sh <machine_id> <num_machine>
-# <machine_id> optional argument to specify the id of the current machine
-# <num_machine> optional argument to specify the total number of machines used
+# usage: ./run.sh <repo_list> <output_folder> <n_merges> <machine_id> <num_machine>
+# <repo_list> list of repositories.
+# <output_folder> folder that contains all outputs.
+# <n_merges> number of merges to sample for each repository.
+# <machine_id> optional argument to specify the id of the current machine.
+# <num_machine> optional argument to specify the total number of machines used.
 # Runs the stack.
-# Takes a long time (days).
-# The output appears in results/ .
+# The output appears in <output_folder> .
 
 
 set -e
 set -o nounset
 
-machine_id="${1:-0}"
-num_machines="${2:-1}"
-
-echo "Machine ID: $machine_id"
-echo "Number of machines: $num_machines"
+REPOS_PATH="$1"
+OUT_DIR="$2"
+N_MERGES=$3
+machine_id="${4:-0}"
+num_machines="${5:-1}"
 
 JAVA_VER=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
 
@@ -24,16 +26,19 @@ if [ "$JAVA_VER" != "8" ]; then
   exit 1
 fi
 
+echo "Machine ID: $machine_id"
+echo "Number of machines: $num_machines"
+
 python3 src/python/get_repos.py
 
-python3 src/python/split_repos.py --repos_path data/repos.csv --machine_id "$machine_id" --num_machines "$num_machines" --output_file results/local_repos.csv
+python3 src/python/split_repos.py --repos_path "$REPOS_PATH" --machine_id "$machine_id" --num_machines "$num_machines" --output_file "$OUT_DIR/local_repos.csv"
 
-python3 src/python/validate_repos.py --repos_path results/local_repos.csv --output_path results/valid_repos.csv
+python3 src/python/validate_repos.py --repos_path "$OUT_DIR/local_repos.csv" --output_path "$OUT_DIR/valid_repos.csv"
 
-./src/scripts/find_merge_commits.sh results/valid_repos.csv results/merges
+./src/scripts/find_merge_commits.sh "$OUT_DIR/valid_repos.csv" "$OUT_DIR/merges"
 
-python3 src/python/parent_merges_test.py --repos_path results/valid_repos.csv --merges_path results/merges/ --output_dir results/merges_valid/ --n_merges 100
+python3 src/python/parent_merges_test.py --repos_path "$OUT_DIR/valid_repos.csv" --merges_path "$OUT_DIR/merges/" --output_dir "$OUT_DIR/merges_valid/" --n_merges "$N_MERGES"
 
-python3 src/python/merge_tester.py --repos_path results/valid_repos.csv --merges_path results/merges_valid/ --output_file results/result.csv
+python3 src/python/merge_tester.py --repos_path "$OUT_DIR/valid_repos.csv" --merges_path "$OUT_DIR/merges_valid/" --output_file "$OUT_DIR/result.csv"
 
-python3 src/python/latex_output.py --result_path results/result.csv --output_path results/plots
+python3 src/python/latex_output.py --result_path "$OUT_DIR/result.csv" --output_path "$OUT_DIR/plots"
--- run_small.sh
+++ run_small.sh
@@ -3,35 +3,13 @@
 # usage: ./run_small.sh
 # Runs the stack on two small repos.
 # The output appears in small/ .
+# <machine_id> optional argument to specify the id of the current machine.
+# <num_machine> optional argument to specify the total number of machines used.
 
 machine_id="${1:-0}"
 num_machines="${2:-1}"
 
-echo "Machine ID: $machine_id"
-echo "Number of machines: $num_machines"
-
 set -e
 set -o nounset
 
-java -version
-
-JAVA_VER=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
-
-if [ "$JAVA_VER" != "8" ]; then
-  echo "Wrong Java version $JAVA_VER. Please use Java 8."
-  exit 1
-fi
-
-python3 src/python/get_repos.py
-
-python3 src/python/split_repos.py --repos_path data/repos_small.csv --machine_id "$machine_id" --num_machines "$num_machines" --output_file small/local_repos_small.csv
-
-python3 src/python/validate_repos.py --repos_path small/local_repos_small.csv --output_path small/valid_repos.csv
-
-./src/scripts/find_merge_commits.sh small/valid_repos.csv small/merges_small
-
-python3 src/python/parent_merges_test.py --repos_path small/valid_repos.csv --merges_path small/merges_small/ --output_dir small/merges_small_valid/ --n_merges 2
-
-python3 src/python/merge_tester.py --repos_path small/valid_repos.csv --merges_path small/merges_small_valid/ --output_file small/result.csv
-
-python3 src/python/latex_output.py --result_path small/result.csv --output_path small/plots
+./run.sh data/repos_small.csv small 2 "$machine_id" "$num_machines"
