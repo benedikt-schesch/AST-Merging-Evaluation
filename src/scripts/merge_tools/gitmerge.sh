c55d5aec852e6bc42b11dad72f93a499a979c199
Ryan Featherman <feathr@cs.washington.edu>
2023-06-12 17:07:34 -0700

    added git merge strategy files

--- src/scripts/merge_tools/gitmerge.sh
+++ src/scripts/merge_tools/gitmerge.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
-# usage: ./gitmerge.sh <clone_dir> <branch-1> <branch-2>
+# usage: ./gitmerge.sh <clone_dir> <branch-1> <branch-2> <strategy>
 # <clone_dir> must contain a clone of a repository.
-# Merges branch2 into branch1, in <clone_dir>.
+# <strategy> is the string to the follow the -s option to git merge,
+# including anything passed through -X
+# Merges branch2 into branch1, in <clone_dir>, using merge strategy <strategy>.
 # Return code is 0 for merge success, 1 for merge failure.
 # For merge failure, also outputs "Conflict" and aborts the merge.
 
 set -e
 set -o nounset
 
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 MERGE_DIR BRANCH1 BRANCH2" >&2
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 MERGE_DIR BRANCH1 BRANCH2 STRATEGY" >&2
   exit 1
 fi
 
 clone_dir=$1
 branch1=$2
 branch2=$3
+strategy=$4
 
 # perform merge
 pushd "$clone_dir"
 git checkout "$branch1" --force
-git merge --no-edit "$branch2"
+git merge --no-edit "$branch2" -s $strategy
 retVal=$?
 
 # report conflicts
