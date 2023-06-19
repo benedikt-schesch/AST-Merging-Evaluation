6cff897fb8f3b71a11f34805458ab67c99e58fd4
Michael Ernst <mernst@cs.washington.edu>
2023-06-18 21:34:40 -0700

    Rename `data/` to `input-data/`

--- src/python/get_repos.py
+++ src/python/get_repos.py
@@ -3,8 +3,9 @@
 
 # usage: python3 get_repos.py
 
-# This script first downloads the reaper dataset.  It then outputs, to file data/repos.csv, the Java
-# repos with more than 10 GitHub stars and a unit_test score of more than 0.25.
+# This script creates file input-data/repos.csv from part of the reaper dataset:
+# the Java repos with more than 10 GitHub stars and a unit_test score of more than 0.25.
+# This script only needs to be re-run when you desire to re-create that file (which is rare).
 
 import gzip
 import urllib.request
@@ -15,21 +16,16 @@ import sys
 import pandas as pd
 import numpy as np
 
-repos_csv = "data/repos.csv"
+repos_csv = "input-data/repos.csv"
 
 if __name__ == "__main__":
-    if os.path.isfile(repos_csv):
-        print("get_repos.py:", repos_csv, "exists; exiting.")
-        sys.exit(0)
-
     urllib.request.urlretrieve(
         "https://reporeapers.github.io/static/downloads/dataset.csv.gz",
         "data/repos.csv.gz",
     )
     with gzip.open("data/repos.csv.gz", "rb") as f:
-        file_content = f.read()
+        df = pd.read_csv(BytesIO(f.read()))
 
-    df = pd.read_csv(BytesIO(file_content))
     df = df[df["language"] == "Java"]
     df = df.replace(to_replace="None", value=np.nan).dropna()
     df = df[df["stars"].astype(int) > 10]
@@ -37,4 +33,4 @@ if __name__ == "__main__":
 
     df.to_csv(repos_csv)
 
-    print("Number of Repos written to", repos_csv, ":", len(df))
+    print("Number of repos written to", repos_csv, ":", len(df))
