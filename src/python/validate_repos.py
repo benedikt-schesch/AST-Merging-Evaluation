a429d37b34e39052fc0dd626a871fecd9d4a48f1
Benedikt schesch <b.schesch@gmail.com>
2023-07-08 07:58:49 -0700

    Reverted some changes

--- src/python/validate_repos.py
+++ src/python/validate_repos.py
@@ -88,21 +88,17 @@ def repo_test(repo_dir_copy: str, timeout: int) -> Tuple[TEST_STATE, str]:
             repo_dir_copy,
         ]
         try:
-            p = subprocess.Popen(  # pylint: disable=consider-using-with
+            p = subprocess.run(  # pylint: disable=consider-using-with
                 command,
-                start_new_session=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                capture_output=True,
+                timeout=timeout,
             )
-            p.wait(timeout=timeout)
         except subprocess.TimeoutExpired:
-            os.killpg(os.getpgid(p.pid), signal.SIGTERM)  # type: ignore
             explanation = "Run Command: " + " ".join(command) + "\nTimed out"
             return TEST_STATE.Tests_timedout, explanation
         rc = p.returncode
-        stdout, stderr = p.communicate()
-        stdout = stdout.decode("utf-8")
-        stderr = stderr.decode("utf-8")
+        stdout = p.stdout.decode("utf-8")
+        stderr = p.stderr.decode("utf-8")
         explanation = (
             "Run Command: "
             + " ".join(command)
