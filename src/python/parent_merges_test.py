de252899821dc6545bc050039db716c7b91aec98
Benedikt <scheschb@student.ethz.ch>
2023-07-10 22:21:58 +0200

    Added lockfile mechanism

--- src/python/parent_merges_test.py
+++ src/python/parent_merges_test.py
@@ -22,6 +22,7 @@ from pathlib import Path
 from functools import partialmethod
 from typing import Tuple, Union, Dict
 import pandas as pd
+import lockfile
 
 from tqdm import tqdm
 from validate_repos import commit_pass_test, del_rw, TEST_STATE
@@ -31,36 +32,70 @@ if os.getenv("TERM", "dumb") == "dumb":
 
 
 WORKDIR = ".workdir/"
+VALID_MERGE_COUNTERS = ".valid_merges/"
 TIMEOUT_TESTING = 30 * 60  # 30 minutes
 
 
+def num_valid_merges(repo_name: str) -> int:
+    """Returns the number of merges that have passing parents for a repository.
+    Args:
+        repo_name (str): The name of the repository.
+    Returns:
+        int: The number of merges that have passing parents for the repository.
+    """
+    lock_file = os.path.join(VALID_MERGE_COUNTERS, repo_name + ".lock")
+    valid_repo_count_dir = os.path.join(VALID_MERGE_COUNTERS, repo_name)
+    with lockfile.LockFile(lock_file, timeout=60):
+        if os.path.isdir(valid_repo_count_dir):
+            with open(valid_repo_count_dir, "r") as f:
+                valid_merge_counter = int(f.read())
+                return valid_merge_counter
+        else:
+            return 0
+
+
+def increment_valid_merges(repo_name: str) -> None:
+    """Increments the number of merges that have passing parents for a repository.
+    Args:
+        repo_name (str): The name of the repository.
+    """
+    lock_file = os.path.join(VALID_MERGE_COUNTERS, repo_name + ".lock")
+    valid_repo_count_dir = os.path.join(VALID_MERGE_COUNTERS, repo_name)
+    with lockfile.LockFile(lock_file, timeout=60):
+        if os.path.isdir(valid_repo_count_dir):
+            with open(valid_repo_count_dir, "r") as f:
+                valid_merge_counter = int(f.read())
+            with open(valid_repo_count_dir, "w") as f:
+                f.write(str(valid_merge_counter + 1))
+        else:
+            with open(valid_repo_count_dir, "w") as f:
+                f.write("1")
+
+
 def parent_pass_test(
-    args: Tuple[str, str, str, str, Union[None, Dict[str, int]], int, str]
+    args: Tuple[str, str, str, str, int, str]
 ) -> Union[Tuple[TEST_STATE, TEST_STATE, TEST_STATE], None]:
     """Indicates whether the two parents of a merge pass tests. Only operates if no more than
         n_sampled other merges have passing parents.
     Args:
-        args (Tuple[str, str, str, str, Union[None, Dict[str, int]], int]): A tuple containing
-            the repository name, the left parent, the right parent, the merge commit, a dictionary
-            containing the number of merges with passing parents for each repository, the
-            maximum number of merges to sample, and the cache directory.
+        args (Tuple[str, str, str, str, int]): A tuple containing
+            the repository name, the left parent, the right parent, the merge commit,
+            the maximum number of merges to sample, and the cache directory.
     Returns:
         Union[Tuple[TEST_STATE, TEST_STATE, TEST_STATE], None]: A tuple containing the test
             results for the left parent, the right parent, and the merge commit, or None if
             enough merges have been sampled.
     """
-    repo_name, left, right, merge, valid_merge_counter, n_sampled, cache_dir = args
-    if not valid_merge_counter is None:
-        if valid_merge_counter[repo_name] > n_sampled:
-            return None
+    repo_name, left, right, merge, n_sampled, cache_dir = args
+    if num_valid_merges(repo_name) >= n_sampled:
+        return TEST_STATE.Not_tested, TEST_STATE.Not_tested, TEST_STATE.Not_tested
     left_test = commit_pass_test(repo_name, left, "left_test", cache_dir)
     if left_test != TEST_STATE.Tests_passed:
         return left_test, TEST_STATE.Not_tested, TEST_STATE.Not_tested
     right_test = commit_pass_test(repo_name, right, "right_test", cache_dir)
     if right_test != TEST_STATE.Tests_passed:
         return left_test, right_test, TEST_STATE.Not_tested
-    if not valid_merge_counter is None:
-        valid_merge_counter[repo_name] = valid_merge_counter[repo_name] + 1
+    increment_valid_merges(repo_name)
     merge_test = commit_pass_test(
         repo_name, merge, f"merge of {left} and {right}", cache_dir
     )
@@ -71,6 +106,7 @@ if __name__ == "__main__":
     print("parent_merges_test: Start")
     Path("repos").mkdir(parents=True, exist_ok=True)
     Path(WORKDIR).mkdir(parents=True, exist_ok=True)
+    Path(VALID_MERGE_COUNTERS).mkdir(parents=True, exist_ok=True)
 
     pwd = os.getcwd()
     parser = argparse.ArgumentParser()
@@ -87,14 +123,12 @@ if __name__ == "__main__":
     os.mkdir(args.output_dir)
 
     multiprocessing_manager = Manager()
-    valid_merge_counter = multiprocessing_manager.dict()
 
     print("parent_merges_test: Constructing Inputs")
     tested_merges = []
     for _, repository_data in tqdm(df.iterrows(), total=len(df)):
         merges_repo = []
         repo_name = repository_data["repository"]
-        valid_merge_counter[repo_name] = 0
         merge_list_file = os.path.join(
             args.merges_path, repo_name.split("/")[1] + ".csv"
         )
@@ -115,7 +149,6 @@ if __name__ == "__main__":
                     merge_data["left"],
                     merge_data["right"],
                     merge_data["merge"],
-                    valid_merge_counter,
                     args.n_merges,
                     args.cache_dir,
                 )
@@ -141,6 +174,7 @@ if __name__ == "__main__":
         r = list(tqdm(pool.imap(parent_pass_test, arguments), total=len(arguments)))
     print("parent_merges_test: Finished Testing")
 
+    shutil.rmtree(VALID_MERGE_COUNTERS, onerror=del_rw)
     print("parent_merges_test: Constructing Output")
     for _, repository_data in tqdm(df.iterrows(), total=len(df)):
         repo_name = repository_data["repository"]
@@ -171,7 +205,6 @@ if __name__ == "__main__":
                     merge_data["left"],
                     merge_data["right"],
                     merge_data["merge"],
-                    None,
                     0,
                     str(args.cache_dir),
                 )
