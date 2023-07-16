4d0c8078989ac32b0c86f6a0bdac51b0e80d2dc2
Benedikt schesch <b.schesch@gmail.com>
2023-07-16 03:02:00 -0700

    Reduced inconsistent merges

---
+++ src/python/delete_inconsistent_merge_results.py
@@ -0,0 +1,57 @@
+"""
+ Delete entries in cache folder that match a certain state.
+"""
+from argparse import ArgumentParser
+import os
+import sys
+import glob
+from tqdm import tqdm
+from latex_output import MERGE_FAILURE_NAMES
+from merge_tester import read_cache, MERGE_TOOLS
+import pandas as pd
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("--results", type=str, default="results/result.csv")
+    arg_parser.add_argument("--cache_path", type=str, default="cache/merge_test_results")
+    args = arg_parser.parse_args()
+
+    files_to_delete = []
+    df = pd.read_csv(args.results)
+    for _, row in tqdm(df.iterrows(), total=len(df)):
+        n_failures = 0
+        for i in MERGE_TOOLS:
+            if row[f"{i}"] in MERGE_FAILURE_NAMES:
+                n_failures += 1
+        if n_failures > 0 and n_failures < len(MERGE_TOOLS):
+            files_to_delete.append(row)
+
+    print("Number of inconsistent entries to delete:", len(files_to_delete))
+    print("Are you sure you want to proceed? (y/n)")
+    if input() != "y":
+        sys.exit(0)
+    for row in tqdm(files_to_delete):
+        for i in MERGE_TOOLS:
+            if row[f"{i}"] in MERGE_FAILURE_NAMES:
+                cache_file = os.path.join(
+                    args.cache_path,
+                    row["repo_name"].split("/")[1]
+                    + "_"
+                    + row["left"]
+                    + "_"
+                    + row["right"]
+                    + "_"
+                    + row["base"]
+                    + "_"
+                    + row["merge"]
+                    + "_"
+                    + i
+                )
+                if os.path.exists(cache_file+".txt"):
+                    os.remove(cache_file+".txt")
+                    if os.path.exists(cache_file+"_explanation.txt"):
+                        os.remove(cache_file+"_explanation.txt")
+                    print("Deleted:", cache_file+".txt")
+                else:
+                    print("File not found:", cache_file)
+    print("Done")
