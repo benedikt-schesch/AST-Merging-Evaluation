72920cb3946df51bf98c9eaa4a5ab5f268ea8a58
Benedikt <b.schesch@gmail.com>
2023-07-23 19:55:18 +0200

    Style Fix

--- src/python/latex_output.py
+++ src/python/latex_output.py
@@ -17,11 +17,11 @@ from pathlib import Path
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 from prettytable import PrettyTable
 from merge_tester import MERGE_TOOL, MERGE_STATE
 from tqdm import tqdm
-import matplotlib
 
 matplotlib.use("pgf")
 matplotlib.rcParams.update(
