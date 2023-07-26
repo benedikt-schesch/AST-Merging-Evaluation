b5d4e30838c3ea0f099752dedd0dae161a0340b5
Benedikt Schesch <b.schesch@gmail.com>
2023-07-26 09:35:24 +0200

    Style fix

--- src/python/latex_output.py
+++ src/python/latex_output.py
@@ -54,7 +54,7 @@ def compute_trivial_merges(df: pd.DataFrame):
         df: dataframe containing the merge results
     """
     trivial_merges = []
-    #return []
+    # return []
     count = 0
     for _, row in tqdm(df.iterrows(), total=len(df)):
         if row["left"] == row["base"] or row["right"] == row["base"]:
