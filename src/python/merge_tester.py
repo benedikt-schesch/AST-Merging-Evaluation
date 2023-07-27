fb89d42a00c193c78e818a3a38470d70caf7a54a
Benedikt schesch <b.schesch@gmail.com>
2023-07-27 10:24:06 -0700

    Bug Fix

--- src/python/merge_tester.py
+++ src/python/merge_tester.py
@@ -63,7 +63,8 @@ MERGE_TOOL = [
     "gitmerge-recursive-histogram",
     "gitmerge-recursive-myers",
     "gitmerge-resolve",
-    "git-hires-merge" "spork",
+    "git-hires-merge",
+    "spork",
     "intellimerge",
 ]
 
