36309d4416bf071cc1ec952d0749ace86b260212
Michael Ernst <mernst@cs.washington.edu>
2023-07-29 21:51:07 -0700

    Edit LaTeX output

--- src/python/latex_output.py
+++ src/python/latex_output.py
@@ -289,11 +289,11 @@ if __name__ == "__main__":
                 100 * incorrect[merge_tool_idx] / total if total != 0 else 0
             )
             table += f"{merge_tool.capitalize()}"
-            table += f" & {correct[merge_tool_idx]} & {correct_percentage:.2f}\\%"
-            table += f" & {unhandled[merge_tool_idx]} & {unhandled_percentage:.2f}\\%"
+            table += f" & {correct[merge_tool_idx]} & {round(correct_percentage)}\\%"
             table += (
-                f" & {incorrect[merge_tool_idx]} & {incorrect_percentage:.2f}\\%\\\\\n"
+                f" & {unhandled[merge_tool_idx]} & {round(unhandled_percentage)}\\%"
             )
+            table += f" & {incorrect[merge_tool_idx]} & {round(incorrect_percentage)}\\%\\\\\n"
         table += "\\end{tabular}\n"
 
         with open(os.path.join(tables_output_path, "table_summary.tex"), "w") as file:
@@ -378,13 +378,13 @@ if __name__ == "__main__":
                 100 * unhandled_feature / len(feature) if len(feature) > 0 else -1
             )
 
-            table2 += f"            {merge_tool.capitalize()}"
-            table2 += f" & {correct_main} & {correct_main_percentage:0.2f}\\%"
-            table2 += f" & {correct_feature} & {correct_feature_percentage:0.2f}\\%"
-            table2 += f" & {unhandled_main} & {unhandled_main_percentage:0.2f}\\%"
-            table2 += f" & {unhandled_feature} & {unhandled_feature_percentage:0.2f}\\%"
-            table2 += f" & {incorrect_main} & {incorrect_main_percentage:0.2f}\\%"
-            table2 += f" & {incorrect_feature} & {incorrect_feature_percentage:0.2f}\\%\\\\ \n"
+            table2 += f"            {merge_tool.capitalize():20}"
+            table2 += f" & {correct_main} & {correct_main_percentage:0.0f}\\%"
+            table2 += f" & {correct_feature} & {correct_feature_percentage:0.0f}\\%"
+            table2 += f" & {unhandled_main} & {unhandled_main_percentage:0.0f}\\%"
+            table2 += f" & {unhandled_feature} & {unhandled_feature_percentage:0.0f}\\%"
+            table2 += f" & {incorrect_main} & {incorrect_main_percentage:0.0f}\\%"
+            table2 += f" & {incorrect_feature} & {incorrect_feature_percentage:0.0f}\\%\\\\ \n"
 
         table2 += "\\end{tabular}\n"
 
@@ -404,7 +404,7 @@ if __name__ == "__main__":
             table3 += f"    {merge_tool.capitalize()}"
             for f in [np.mean, np.median, np.max]:
                 run_time = f(result_df[merge_tool + " run_time"])
-                table3 += f" & {run_time:0.2f}"
+                table3 += f" & {round(run_time)}"
             table3 += "\\\\\n"
         table3 += "\\end{tabular}\n"
 
