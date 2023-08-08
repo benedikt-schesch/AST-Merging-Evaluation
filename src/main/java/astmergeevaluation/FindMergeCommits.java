120d12f605a685effe9d2b002da80b35d6ebaa1a
Michael Ernst <mernst@cs.washington.edu>
2023-08-08 09:42:23 -0700

    More directory name changes

--- src/main/java/astmergeevaluation/FindMergeCommits.java
+++ src/main/java/astmergeevaluation/FindMergeCommits.java
@@ -263,7 +263,7 @@ public class FindMergeCommits {
   void writeMergeCommits(OrgAndRepo orgAndRepo) throws IOException, GitAPIException {
     String orgName = orgAndRepo.org;
     String repoName = orgAndRepo.repo;
-    File outputFile = new File(outputDir.toFile(), orgName + "__" + repoName + ".csv");
+    File outputFile = new File(outputDir.toFile(), repoName + ".csv");
     Path outputPath = outputFile.toPath();
     if (Files.exists(outputPath)) {
       // File exists, so there is nothing to do.
