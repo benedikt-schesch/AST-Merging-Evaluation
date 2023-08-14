7887623fce88dc2534af5d2f8a4eb4c832bb6108
Benedikt Schesch <b.schesch@gmail.com>
2023-08-14 14:15:09 +0200

    Added tool

--- src/python/repo.py
+++ src/python/repo.py
@@ -41,6 +41,8 @@ MERGE_TOOL = Enum(
     [
         "gitmerge_ort",
         "gitmerge_ort_ignorespace",
+        "gitmerge_ort_imports",
+        "gitmerge_ort_imports_ignorespace",
         "gitmerge_recursive_patience",
         "gitmerge_recursive_minimal",
         "gitmerge_recursive_histogram",
