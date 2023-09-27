ac5fd880b7267f57afe2acd21dea13e4fe0c3f4d
Michael Ernst <mernst@cs.washington.edu>
2023-09-27 11:54:51 -0700

    Make resolve-import-conflicts return a status code (#218)

--- src/scripts/merge_tools/gitmerge_ort_imports.sh
+++ src/scripts/merge_tools/gitmerge_ort_imports.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env sh
 
-# usage: ./gitmerge-ort.sh <clone_dir> <branch-1> <branch-2>
+# usage: ./gitmerge_ort_imports.sh <clone_dir> <branch-1> <branch-2>
 
 MERGE_SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 clone_dir=$1
 branch1=$2
 branch2=$3
 strategy="-s ort"
-"$MERGE_SCRIPTS_DIR"/gitmerge.sh "$clone_dir" "$branch1" "$branch2" "$strategy"
+if "$MERGE_SCRIPTS_DIR"/gitmerge.sh "$clone_dir" "$branch1" "$branch2" "$strategy"; then
+  exit 0
+fi
 
-(cd "$clone_dir" && "$MERGE_SCRIPTS_DIR"/resolve-import-conflicts)
+cd "$clone_dir" || exit 1
+if ! "$MERGE_SCRIPTS_DIR"/resolve-import-conflicts; then
+  echo "Conflict"
+  git merge --abort
+  exit 1
+fi
+
+exit 0
