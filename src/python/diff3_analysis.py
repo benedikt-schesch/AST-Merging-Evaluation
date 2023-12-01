d5c8a47eb4a42d2dbbbddc331b0f4d2c52bf2018
cactusbranch01 <benradroberts@gmail.com>
2023-12-01 14:09:21 -0800

    Update to Diff3 Analysis Multiple Conflicts

--- src/python/diff3_analysis.py
+++ src/python/diff3_analysis.py
@@ -48,35 +48,35 @@ def diff3_analysis(merge_tool: str, repo_num: int):
         stdout=subprocess.PIPE,
         text=True,
     )
-    conflict_file_match = re.search(
+
+    conflict_file_matches = re.findall(
         r"CONFLICT \(.+\): Merge conflict in (.+)", result.stdout
     )
 
-    if conflict_file_match:
-        conflicting_file = conflict_file_match.group(1)
+    repo = clone_repo_to_path(
+        repo_name, "./repos/programmer_merge"
+    )  # Return a Git-Python repo object
+    repo.git.checkout(df.iloc[repo_num]["merge"], force=True)
+    repo.submodule_update()
+
+    repo = clone_repo_to_path(
+        repo_name, "./repos/base"
+    )  # Return a Git-Python repo object
+    repo.git.checkout(df.iloc[repo_num]["base"], force=True)
+    repo.submodule_update()
+
+    for conflict_file_match in conflict_file_matches:
+        conflicting_file = str(conflict_file_match)
         conflict_path = os.path.join(repo_name, conflicting_file)
         conflict_path_merge_attempt = os.path.join(
             "./repos/merge_attempt", conflict_path
         )
 
-        repo = clone_repo_to_path(
-            repo_name, "./repos/base"
-        )  # Return a Git-Python repo object
-        repo.remote().fetch()
-        repo.git.checkout(df.iloc[repo_num]["base"], force=True)
-        repo.submodule_update()
         conflict_path_base = os.path.join("./repos/base", conflict_path)
 
-        repo = clone_repo_to_path(
-            repo_name, "./repos/programmer_merge"
-        )  # Return a Git-Python repo object
-        repo.remote().fetch()
-        repo.git.checkout(df.iloc[repo_num]["merge"], force=True)
-        repo.submodule_update()
         conflict_path_programmer_merge = os.path.join(
             "./repos/programmer_merge", conflict_path
         )
-
         diff_results = subprocess.run(
             [
                 "diff3",
@@ -85,30 +85,27 @@ def diff3_analysis(merge_tool: str, repo_num: int):
                 conflict_path_programmer_merge,
             ],
             stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
             text=True,
         )
+
+        # Check for the specific error message
+        error_message = "No such file or directory"
+        if error_message in diff_results.stderr:
+            # Run diff in the other way
+            diff_results = subprocess.run(
+                [
+                    "diff",
+                    conflict_path_merge_attempt,
+                    conflict_path_programmer_merge,
+                ],
+                stdout=subprocess.PIPE,
+                text=True,
+            )
 
         # Use a temporary file to store the diff results
         with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_file:
             temp_file.write(diff_results.stdout)
-
-        # Deletes base, programmer_merge, and merge_attempt folders in repos dir
-        # We do this to prevent errors if cloning the same repo into the folder twice
-        subprocess.run(
-            ["rm", "-rf", "./repos/base"],
-            stderr=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-        )
-        subprocess.run(
-            ["rm", "-rf", "./repos/merge_attempt"],
-            stderr=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-        )
-        subprocess.run(
-            ["rm", "-rf", "./repos/programmer_merge"],
-            stderr=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-        )
 
         # Open the saved text file with the default application
         subprocess.run(["xdg-open", temp_file.name], check=True)
@@ -116,5 +113,21 @@ def diff3_analysis(merge_tool: str, repo_num: int):
         # Delete the temporary file
         os.remove(temp_file.name)
 
-    else:
-        print("No conflicting file found in the output.")
+    print("reached delete repo")
+    # Deletes base, programmer_merge, and merge_attempt folders in repos dir
+    # We do this to prevent errors if cloning the same repo into the folder twice
+    subprocess.run(
+        ["rm", "-rf", "./repos/base"],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    subprocess.run(
+        ["rm", "-rf", "./repos/merge_attempt"],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+    subprocess.run(
+        ["rm", "-rf", "./repos/programmer_merge"],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
