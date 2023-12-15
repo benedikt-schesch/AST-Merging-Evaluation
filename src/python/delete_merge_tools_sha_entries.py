d7ff98b53cc3a5b6e96fbc89bbfc89fdadb17d47
Benedikt Schesch <b.schesch@gmail.com>
2023-12-15 11:08:12 -0800

    Delete merge tool entries update

--- src/python/delete_merge_tools_sha_entries.py
+++ src/python/delete_merge_tools_sha_entries.py
@@ -6,19 +6,36 @@ cache/sha_cache_entry directory and subdirectories.
 import json
 from pathlib import Path
 
-base_path = Path("cache/sha_cache_entry")
-deleted = 0
-# Iterate over all json files in the directory and subdirectories
-for json_file in base_path.glob("**/*.json"):
-    # Load json file
-    with json_file.open() as f:
-        data = json.load(f)
-    # Delete all entries that finish with "intellimerge"
-    for key in list(data.keys()):
-        if len(key) > 41 and "left_fingerprint" not in data[key]:
-            deleted += 1
-            del data[key]
-    # Save json file
-    with json_file.open("w") as f:
-        json.dump(data, f)
-print(f"Deleted {deleted} entries.")
+
+def delete_merge_tool_entries(delete: bool = False):
+    """Delete all entries that finish with "intellimerge" in the
+    cache/sha_cache_entry directory and subdirectories.
+    """
+    base_path = Path("cache/sha_cache_entry")
+    n_deleted = 0
+    # Iterate over all json files in the directory and subdirectories
+    for json_file in base_path.glob("**/*.json"):
+        # Load json file
+        with json_file.open() as f:
+            data = json.load(f)
+        # Delete all entries that finish with "intellimerge"
+        for key in list(data.keys()):
+            if len(key) > 41 and "merge status" not in data[key]:
+                n_deleted += 1
+                if delete:
+                    del data[key]
+        # Save json file
+        if delete:
+            with json_file.open("w") as f:
+                json.dump(data, f)
+    return n_deleted
+
+
+if __name__ == "__main__":
+    deleted = delete_merge_tool_entries(delete=False)
+    print("Total entries to be deleted: ", deleted)
+    # Confirm deletion
+    confirm = input("Confirm deletion? (y/n) ")
+    if confirm == "y":
+        deleted = delete_merge_tool_entries(delete=True)
+    print(f"Deleted {deleted} entries.")
