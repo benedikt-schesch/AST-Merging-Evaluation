1bee58a1d7603ec971286acd6b7b666954780233
Benedikt Schesch <b.schesch@gmail.com>
2024-04-24 23:28:39 -0700

    Bug fix

--- src/scripts/merge_tools/gitmerge_ort_imports.sh
+++ src/scripts/merge_tools/gitmerge_ort_imports.sh
@@ -8,7 +8,7 @@ branch1=$2
 branch2=$3
 strategy="-s ort"
 
-export JAVA_HOME=$JAVA17_HOME
+export JAVA_HOME="$JAVA17_HOME"
 
 cd "$clone_dir" || exit 1
 
@@ -16,10 +16,5 @@ attributes_file=".git/info/attributes"
 echo "*.java merge=merge-java" >> "$attributes_file"
 
 git config --local merge.merge-java.name "Merge Java files"
-git config --local merge.merge-java.driver "java-merge-driver.sh "%A" "%O" "%B""
-
-if "$MERGE_SCRIPTS_DIR"/gitmerge.sh "$clone_dir" "$branch1" "$branch2" "$strategy"; then
-  exit 0
-fi
-
-exit 0
+git config --local merge.merge-java.driver 'java-merge-driver.sh "%A" "%O" "%B"'
+"$MERGE_SCRIPTS_DIR"/gitmerge.sh "$clone_dir" "$branch1" "$branch2" "$strategy"
