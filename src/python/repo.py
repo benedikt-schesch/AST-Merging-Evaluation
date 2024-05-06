ca10ec796f6a9477cf46accd9fb8242ec0c97dcd
cactusbranch01 <benradroberts@gmail.com>
2024-05-06 15:38:14 -0700

    Updated clone_repo docs

--- src/python/repo.py
+++ src/python/repo.py
@@ -95,7 +95,8 @@ def clone_repo(repo_slug: str, repo_dir: Path) -> git.repo.Repo:
 
 @timeout(10 * 60)
 def clone_repo_to_path(repo_name: str, path: str) -> git.repo.Repo:
-    """Clones a repository to a specified path.
+    """Clones a repository to a specified path. Alternative to the clone_repo 
+    method since it takes a string path and returns a git repo Repo obj.
 
     Args:
         repo_name (str): The name of the repository in the format "owner/reponame".
