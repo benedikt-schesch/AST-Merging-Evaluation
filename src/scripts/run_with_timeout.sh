d3a848bfc4e3beb8e2499f04bce84288384d7fab
Benedikt Schesch <b.schesch@gmail.com>
2024-05-27 11:48:33 -0700

    Fix style

--- src/scripts/run_with_timeout.sh
+++ src/scripts/run_with_timeout.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# usage: ./run_with_timeout.sh <timeout_in_seconds> <command>
+# Run a command with a timeout. The exit status is 0 for success, 124 for timeout, and 1 for failure.
 
 # Check if the correct number of arguments was provided
 if [ "$#" -ne 2 ]; then
@@ -9,14 +11,12 @@ fi
 # Read the timeout and command from arguments
 TIMEOUT=$1
 shift
-COMMAND=$@
+COMMAND=$*
 
 # Function to run the command with a timeout
 run_command_with_timeout() {
-    # Using the timeout command to manage the execution time
-    # '--foreground' allows it to kill the process group of a foreground job
-    # '--kill-after' ensures the process group is killed if it doesn't exit within a grace period after the timeout
-    timeout --kill-after=10 $TIMEOUT $COMMAND
+    # shellcheck disable=SC2086
+    timeout --kill-after=10 "$TIMEOUT" $COMMAND
     local STATUS=$?
 
     # Check if the command timed out
