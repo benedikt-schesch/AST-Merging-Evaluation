62546738511635120af0c095b72c9aacca0e364e
Michael Ernst <mernst@cs.washington.edu>
2024-09-01 14:31:26 -0700

    Further reduce diffs

--- src/scripts/merge_tools/merge_plumelib.sh
+++ src/scripts/merge_tools/merge_plumelib.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 
-# usage: ./gitmerge_ort_imports_ignorespace.sh <clone_dir> <branch-1> <branch-2> <git_strategy> <merge_strategy>
+# usage: ./merge_plumelib.sh <clone_dir> <branch-1> <branch-2> <git_strategy> <merge_strategy>
+# First runs `git merge`, then runs Plume-lib Merging to improve the ruselt of `git merge`.
+# <clone_dir> must contain a clone of a repository.
+# Return code is 0 for merge success, 1 for merge failure, 2 for script failure.
+# For merge failure, also outputs "Conflict" and aborts the merge.
+
+set -o nounset
+
+if [ "$#" -ne 5 ]; then
+  echo "Usage: $0 CLONE_DIR BRANCH1 BRANCH2 GIT_STRATEGY MERGE_STRATEGY" >&2
+  exit 2
+fi
 
 clone_dir=$1
 branch1=$2
@@ -8,21 +19,25 @@ branch2=$3
 git_strategy=$4 #"-Xignore-space-change"
 merge_strategy=$5 #"--only-adjacent"
 
+## Perform merge
+
 echo "$0: Merging $branch1 and $branch2 with git_strategy=$git_strategy and merge_strategy=$merge_strategy"
 
-cd "$clone_dir" || (echo "$0: cannot cd to $clone_dir" ; exit 1)
+cd "$clone_dir" || (echo "$0: cannot cd to $clone_dir" ; exit 2)
 
 git checkout "$branch1" --force
 git config --local merge.conflictstyle diff3
-
 git config --local mergetool.prompt false
+
+# shellcheck disable=SC2086
+git merge --no-edit $git_strategy "$branch2"
+
+## Now, run Plume-lib Merging to improve the result of `git merge`.
+
 git config --local merge.tool merge-plumelib
 # shellcheck disable=SC2016
 git config --local mergetool.merge-plumelib.cmd 'java-merge-tool.sh '"$merge_strategy"' ${BASE} ${LOCAL} ${REMOTE} ${MERGED}'
 git config --local mergetool.merge-plumelib.trustExitCode true
-
-# shellcheck disable=SC2086
-git merge --no-edit $git_strategy "$branch2"
 
 case "$merge_strategy" in
     *"--no-imports"* | *"--only-adjacent"* | *"--only-annotations"* | *"--only-version-numbers"*)
