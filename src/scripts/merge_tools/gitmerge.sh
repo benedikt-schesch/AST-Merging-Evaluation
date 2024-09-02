18dd236cdafbe0bc45967d08efa051b97da03b38
Michael Ernst <mernst@cs.washington.edu>
2024-09-01 19:36:57 -0700

    Tweak git settings

--- src/scripts/merge_tools/gitmerge.sh
+++ src/scripts/merge_tools/gitmerge.sh
@@ -24,7 +24,8 @@ strategy=$4
 cd "$clone_dir" || (echo "$0: cannot cd to $clone_dir" ; exit 1)
 
 git checkout "$branch1" --force
-git config merge.conflictstyle zdiff3
+git config --local merge.conflictstyle zdiff3
+git config --local mergetool.prompt false
 
 echo "Running: git merge --no-edit $strategy $branch2"
 # shellcheck disable=SC2086
