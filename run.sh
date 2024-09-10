2f76bbdb8f94eb422f1a22e16a8e8c06369e16fb
Benedikt <b.schesch@gmail.com>
2024-09-10 22:26:37 +0200

    Added laCompile with GraalVM

--- run.sh
+++ run.sh
@@ -125,7 +125,7 @@ if [ ! -f ./src/scripts/merge_tools/merging/.git ] ; then
     git submodule update --init --recursive
 fi
 
-(cd ./src/scripts/merge_tools/merging && JAVA_HOME=$JAVA_GRAALVM_HOME ./gradlew nativeCompile)
+(cd ./src/scripts/merge_tools/merging && JAVA_HOME=$GRAALVM_HOME PATH="$JAVA_HOME/bin:$PATH" ./gradlew nativeCompile)
 
 echo "Machine ID: $machine_id"
 echo "Number of machines: $num_machines"
