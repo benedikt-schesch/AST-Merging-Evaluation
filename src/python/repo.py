9105f33e90ef96cc9ee4691764a23c3eb51293a6
Benedikt Schesch <b.schesch@gmail.com>
2024-09-17 00:17:16 -0700

    Added latest data

--- src/python/repo.py
+++ src/python/repo.py
@@ -120,12 +120,9 @@ MERGE_TOOL = Enum(
         "git_hires_merge",
         "spork",
         "intellimerge",
-        # The below is mostly "X + plume-lib-merging-fixups",
-        # except that these are not:
-        #  * "adjacent"
-        #  * "imports"
-        #  * "version_numbers"
-        # TODO: move those above this comment, then adjust the comment.
+        "adjacent",
+        "imports",
+        "version_numbers",
         "plumelib_git_hires_merge",
         "plumelib_intellimerge",
         "plumelib_recursive_histogram",
@@ -137,9 +134,6 @@ MERGE_TOOL = Enum(
         "plumelib_spork",
         "plumelib_ort",
         "plumelib_ort_ignorespace",
-        "adjacent",
-        "imports",
-        "version_numbers",
     ],
 )
 MERGE_STATE = Enum(
--- src/scripts/merge_tools/plumelib_adjacent.sh
+++ src/scripts/merge_tools/plumelib_adjacent.sh
@@ -7,7 +7,5 @@ clone_dir=$1
 branch1=$2
 branch2=$3
 git_strategy="-s ort"
-# Uses both --adjacent and all of plume-lib merging's default behaviors.
-# Thus, this implements "adjacent + plume-lib-merging-fixups".
 plumelib_strategy="--adjacent"
 "$MERGE_SCRIPTS_DIR"/merge_git_then_plumelib.sh "$clone_dir" "$branch1" "$branch2" "$git_strategy" "$plumelib_strategy"
