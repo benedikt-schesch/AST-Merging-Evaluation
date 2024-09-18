5bed1e87a52361af7e8b7d155973b95dc7253407
Michael Ernst <mernst@cs.washington.edu>
2024-09-18 06:41:17 -0700

    Rename tools

--- src/scripts/merge_tools/gitmerge_recursive_patience_plus.sh
+++ src/scripts/merge_tools/gitmerge_recursive_patience_plus.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-# usage: ./plumelib_recursive_myers.sh <clone_dir> <branch-1> <branch-2>
+# usage: ./gitmerge_recursive_patience_plus.sh <clone_dir> <branch-1> <branch-2>
 
 MERGE_SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 clone_dir=$1
 branch1=$2
 branch2=$3
-git_strategy="-s recursive -Xdiff-algorithm=myers"
+git_strategy="-s recursive -Xdiff-algorithm=patience"
 plumelib_strategy=""
 "$MERGE_SCRIPTS_DIR"/merge_git_then_plumelib.sh "$clone_dir" "$branch1" "$branch2" "$git_strategy" "$plumelib_strategy"
--- src/scripts/merge_tools/gitmerge_resolve_plus.sh
+++ src/scripts/merge_tools/gitmerge_resolve_plus.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-# usage: ./plumelib_recursive_histogram.sh <clone_dir> <branch-1> <branch-2>
+# usage: ./gitmerge_resolve_plus.sh <clone_dir> <branch-1> <branch-2>
 
 MERGE_SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 clone_dir=$1
 branch1=$2
 branch2=$3
-git_strategy="-s recursive -Xdiff-algorithm=histogram"
+git_strategy="-s resolve"
 plumelib_strategy=""
 "$MERGE_SCRIPTS_DIR"/merge_git_then_plumelib.sh "$clone_dir" "$branch1" "$branch2" "$git_strategy" "$plumelib_strategy"
