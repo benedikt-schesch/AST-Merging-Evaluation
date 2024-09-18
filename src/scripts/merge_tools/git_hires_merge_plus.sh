f27dc99ca92b7e656ec2542edc0bb753a553f394
Michael Ernst <mernst@cs.washington.edu>
2024-09-17 17:58:32 -0700

    Rename tools

--- src/scripts/merge_tools/git_hires_merge_plus.sh
+++ src/scripts/merge_tools/git_hires_merge_plus.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-# usage: ./plumelib_intellimerge.sh <clone_dir> <branch-1> <branch-2>
+# usage: ./git_hires_merge_plus.sh <clone_dir> <branch-1> <branch-2>
 
 MERGE_SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 clone_dir=$1
 branch1=$2
 branch2=$3
-merge_script="intellimerge.sh"
+merge_script="git_hires_merge.sh"
 plumelib_strategy=""
 "$MERGE_SCRIPTS_DIR"/merge_script_then_plumelib.sh "$clone_dir" "$branch1" "$branch2" "$merge_script" "$plumelib_strategy"
