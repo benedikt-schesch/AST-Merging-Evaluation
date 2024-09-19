dc49ce29af537ac218f4784acf040f178cc852fb
Michael Ernst <mernst@cs.washington.edu>
2024-09-19 09:45:53 -0700

    Add a complete set of "_plus" scripts

--- src/scripts/merge_tools/gitmerge_recursive_ignorespace.sh
+++ src/scripts/merge_tools/gitmerge_recursive_ignorespace.sh
@@ -2,9 +2,9 @@
 
 # usage: ./gitmerge_recursive_ignorespace.sh <clone_dir> <branch-1> <branch-2>
 
-MERGE_DIR="$(dirname "$0")"
+MERGE_SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 clone_dir=$1
 branch1=$2
 branch2=$3
 git_strategy="-s recursive -Xignore-space-change"
-"$MERGE_DIR"/gitmerge.sh "$clone_dir" "$branch1" "$branch2" "$git_strategy"
+"$MERGE_SCRIPTS_DIR"/gitmerge.sh "$clone_dir" "$branch1" "$branch2" "$git_strategy"
